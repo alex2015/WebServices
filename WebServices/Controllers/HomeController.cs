ef314751cb98b7ab9e2cebd4a35c2c41bf4c0bd7
alex2015 <alex2015@rambler.ru>
2017-05-11 22:08:10 +0300

    add HomeController and views

---
+++ WebServices/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using WebServices.Models;
+using System.Web.Mvc;
+
+namespace WebServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        ReservationRepository repository = ReservationRepository.Current;
+        public ViewResult Index()
+        {
+            return View(repository.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Add(item);
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult Update(Reservation item)
+        {
+            if (ModelState.IsValid && repository.Update(item))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult Remove(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Remove(id);
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
+    }
+}
