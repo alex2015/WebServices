07a45652f2d957cf8397396314e24174319eb897
alex2015 <alex2015@rambler.ru>
2017-05-12 18:27:02 +0300

    commit

--- WebServices/Controllers/HomeController.cs
+++ WebServices/Controllers/HomeController.cs
@@ -1,46 +1,12 @@
-﻿using WebServices.Models;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WebServices.Controllers
 {
     public class HomeController : Controller
     {
-        ReservationRepository repository = ReservationRepository.Current;
         public ViewResult Index()
         {
-            return View(repository.GetAll());
-        }
-
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                repository.Add(item);
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
-        }
-
-        public ActionResult Update(Reservation item)
-        {
-            if (ModelState.IsValid && repository.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
-        }
-
-        public ActionResult Remove(int id)
-        {
-            if (ModelState.IsValid)
-            {
-                repository.Remove(id);
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
+            return View();
         }
     }
 }
